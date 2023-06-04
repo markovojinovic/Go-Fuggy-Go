1b035ca7799674e4ed4c24fb189313894227ae12
theOriginalFelto <milos.kovacevic2000@gmail.com>
2023-06-04 10:44:42 +0200

    Dodati bubble i sidro, ima neki nullptr izuzetak u Unitiju

--- Assets/Script/FuggyController.cs
+++ Assets/Script/FuggyController.cs
@@ -3,9 +3,11 @@ using TMPro;
 
 public class FuggyController : MonoBehaviour
 {
-    public Vector2 initialVelocity;
+    public enum FuggyState { DEPUMPED, PUMPED, IN_BUBBLE }
+
+    public Vector2 fuggyVelocity;
     public Camera ourCamera;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 0;
     public float idleVerticalSpeed = -1f;
     public int score = 0;
     private int scoreIncrement = 1;
@@ -13,6 +15,7 @@ public class FuggyController : MonoBehaviour
     private int changeBound = 10;
     private Animator animator;
     public RuntimeAnimatorController pumpAnimationController, depumpAnimationController;
+    public GameObject gameOverText;
 
     public TextMeshProUGUI tmpText;
 
@@ -22,6 +25,10 @@ public class FuggyController : MonoBehaviour
     private float boundaryRight;
     private Vector3 cameraVelocity;
     private CircleCollider2D circleCollider;
+    private FuggyState fuggyState = FuggyState.DEPUMPED;
+    private GameObject bubbleGameObject;
+    private const float MOVE_RIGHT = 5f;
+    private const float MOVE_LEFT = -5f;
 
     private void Start()
     {
@@ -30,7 +37,7 @@ public class FuggyController : MonoBehaviour
         pumpAnimationController = Resources.Load<RuntimeAnimatorController>("Assets/Animations/FuggyAnimationDepumpSprite_0.controller");
         depumpAnimationController = Resources.Load<RuntimeAnimatorController>("Assets/Animations/FuggyAnimationPumpSprite_0.controller");
         // animator.runtimeAnimatorController = pumpAnimationController;
-        initialVelocity = new Vector2(0f, idleVerticalSpeed);
+        fuggyVelocity = new Vector2(0f, idleVerticalSpeed);
         cameraVelocity = new Vector3(0f, idleVerticalSpeed);
         rb = GetComponent<Rigidbody2D>();
         circleCollider = GetComponent<CircleCollider2D>();
@@ -48,36 +55,51 @@ public class FuggyController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && spaceEnabled){
             // animator.enabled = true;
             // animator.Play("FuggyPumpAnimation");
-            idleVerticalSpeed = -0.7f;
-            initialVelocity = new Vector2(0f, idleVerticalSpeed);
-            cameraVelocity = new Vector3(0f, idleVerticalSpeed);
+            fuggyState = FuggyState.PUMPED;
             Invoke("DisableSpace", 7f);
         } else if(Input.GetKeyUp(KeyCode.Space) && spaceEnabled){
             // animator.runtimeAnimatorController = depumpAnimationController;
             // animator.Play("FuggyDepumpAnimation");
             CancelInvoke("DisableSpace");
             spaceEnabled = false;
-            idleVerticalSpeed = -1f;
-            initialVelocity = new Vector2(0f, idleVerticalSpeed);
-            cameraVelocity = new Vector3(0f, idleVerticalSpeed);
+            fuggyState = FuggyState.DEPUMPED;
             Invoke("EnableSpace", 5f);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-            initialVelocity.x = -moveSpeed;
-        else if (Input.GetKeyUp(KeyCode.LeftArrow))
-            initialVelocity.x = 0;
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            initialVelocity.x = moveSpeed;
-        else if (Input.GetKeyUp(KeyCode.RightArrow))
-            initialVelocity.x = 0;
-
-        Vector2 newFuggyPosition = rb.position + initialVelocity * Time.deltaTime;
+        constructVelocities();
+        Vector2 newFuggyPosition = rb.position + fuggyVelocity * Time.deltaTime;
         newFuggyPosition.x = Mathf.Clamp(newFuggyPosition.x, boundaryLeft, boundaryRight);
         rb.position = newFuggyPosition;
+        if (bubbleGameObject)
+            bubbleGameObject.transform.position = newFuggyPosition;
 
         ourCamera.transform.position += cameraVelocity * Time.deltaTime;
+    }
+
+    private void constructVelocities() {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            moveSpeed = MOVE_LEFT;
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+            moveSpeed = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            moveSpeed = MOVE_RIGHT;
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+            moveSpeed = 0;
+
+        switch (fuggyState) {
+            case FuggyState.DEPUMPED:
+                idleVerticalSpeed = -1f;
+                break;
+            case FuggyState.PUMPED:
+                idleVerticalSpeed = -0.7f;
+                break;
+            case FuggyState.IN_BUBBLE:
+                idleVerticalSpeed = 1f;
+                break;
+        }
+        fuggyVelocity = new Vector2(moveSpeed, idleVerticalSpeed);
+        cameraVelocity = new Vector3(0, idleVerticalSpeed);
     }
 
     private void EnableSpace() {
@@ -87,7 +109,7 @@ public class FuggyController : MonoBehaviour
     private void DisableSpace() {
         spaceEnabled = false;
         idleVerticalSpeed = -1f;
-        initialVelocity = new Vector2(0f, idleVerticalSpeed);
+        fuggyVelocity = new Vector2(0f, idleVerticalSpeed);
         cameraVelocity = new Vector3(0f, idleVerticalSpeed);
         Invoke("EnableSpace", 5f);
     }
@@ -112,5 +134,34 @@ public class FuggyController : MonoBehaviour
             timeScoreUpdate -= 0.05;
 
         Invoke("IncrementScore", (float)timeScoreUpdate);
+    }
+
+    private void resetBubbleState() { 
+        this.fuggyState = FuggyState.DEPUMPED;
+        Destroy(this.bubbleGameObject);
+        this.bubbleGameObject = null; 
+    }
+
+    public FuggyState getFuggyState() { return this.fuggyState; }
+
+    public void setFuggyStateInBubble(GameObject bubble) {
+        fuggyState = FuggyState.IN_BUBBLE;
+        bubbleGameObject = bubble;
+        bubbleGameObject.transform.position = rb.position;
+        Invoke("resetBubbleState", Random.Range(3f, 4f));
+    }
+
+    public void StopRendering()
+    {
+        Invoke("quitGame", 2f);
+        Instantiate(gameOverText, new Vector3(0f, 0f, 0f), Quaternion.identity);
+    }
+
+    private void quitGame(){
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
     }
 }
